2c65ec4474ca9e7b0f2986924229e66aae4631f7
leelaprsth-cursed <leelaprsth@gmail.com>
2026-03-31 19:58:34 +0530

    Implement smart number analyzer
    Added a smart number analyzer that checks if a number is even or odd, positive or negative, and compares it with 10.

--- week-01-python-basics/day-02/code.py
+++ week-01-python-basics/day-02/code.py
@@ -1,1 +1,25 @@
+# Day 2 - Smart Number Analyzer
 
+num = int(input("Enter a number: "))
+
+# Even or Odd
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+# Positive / Negative
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+# Compare with 10
+if num > 10:
+    print("Greater than 10")
+elif num == 10:
+    print("Equal to 10")
+else:
+    print("Less than 10")
