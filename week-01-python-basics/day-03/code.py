b355c3a3d4810de0ddb2672ad7e8e7780786f52e
leelaprsth-cursed <leelaprsth@gmail.com>
2026-04-01 19:49:25 +0530

    Add loops for printing numbers and squares

--- week-01-python-basics/day-03/code.py
+++ week-01-python-basics/day-03/code.py
@@ -1,1 +1,19 @@
+for i in range(1, 11):
+    print(i)
 
+print()
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+print()
+
+for i in range(1, 11):
+    print(i ** 2)
+
+print()
+
+n = int(input("Enter a number: "))
+for i in range(1, n + 1):
+    print(i)
