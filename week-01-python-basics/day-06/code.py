d031acabf3a9ebbd462d2cbc6f256239a213b49e
leelaprsth-cursed <leelaprsth@gmail.com>
2026-04-04 20:06:37 +0530

    Add patterns for square, right triangle, and number triangle

--- week-01-python-basics/day-06/code.py
+++ week-01-python-basics/day-06/code.py
@@ -1,1 +1,19 @@
+n = int(input("Enter a number: "))
 
+print("\nPattern 1: Square")
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print("*", end=' ')
+    print()
+
+print("\nPattern 2: Right Triangle")
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print("*", end=' ')
+    print()
+
+print("\nPattern 3: Number Triangle")
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print()
